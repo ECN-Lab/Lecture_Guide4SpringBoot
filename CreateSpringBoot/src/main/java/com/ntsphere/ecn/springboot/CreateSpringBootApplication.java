23265407e7ab353a4fb0536398a5f08a1fadcc31
ECN <reinlainer@gmail.com>
2019-11-25 12:51:28 +0900

    Initial commit

--- CreateSpringBoot/src/main/java/com/ntsphere/ecn/springboot/CreateSpringBootApplication.java
+++ CreateSpringBoot/src/main/java/com/ntsphere/ecn/springboot/CreateSpringBootApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LectureSpringBootApplication {
+public class CreateSpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LectureSpringBootApplication.class, args);
+		SpringApplication.run(CreateSpringBootApplication.class, args);
 	}
 
 }
